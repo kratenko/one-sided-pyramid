9673317f5245c8e3ee2a5698cd1acdd0b8738f0f
kratenko <kratenko@garstig.org>
2016-01-22 22:30:47 +0100

    Pyramid starter scaffold

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_mako',
+    'pyramid_debugtoolbar',
+    'waitress',
+]
+
+setup(name='one-sided-pyramid',
+      version='0.0.1',
+      description='one-sided-pyramid',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="one_sided_pyramid",
+      entry_points="""\
+      [paste.app_factory]
+      main = one_sided_pyramid:main
+      """,
+      )
